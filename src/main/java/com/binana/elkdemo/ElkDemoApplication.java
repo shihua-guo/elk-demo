f470235865ca762495a8e792c33a0f6328b280d5
guoshihua <2977131415@qq.com>
2020-02-28 16:03:34 +0800

    [初始化]添加注释

--- src/main/java/com/binana/elkdemo/ElkDemoApplication.java
+++ src/main/java/com/binana/elkdemo/ElkDemoApplication.java
@@ -1,13 +1,18 @@
 package com.binana.elkdemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ElkDemoApplication {
-
+    private static Logger logger = LoggerFactory.getLogger("elk_logger");
     public static void main(String[] args) {
         SpringApplication.run(ElkDemoApplication.class, args);
+        logger.info("我是一个简易的spring-boot连接ELK的应用");
+        logger.warn("DSADDSDSD");
+
     }
 
 }
